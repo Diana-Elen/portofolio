cf702572eb87bd6169bdd25af70d5f92d8167a77
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-14 22:26:47 +0300

    add navbar

--- my-portofolio/components/Button.js
+++ my-portofolio/components/Button.js
@@ -1,6 +1,6 @@
 export default function Button() {
     return <>
-    <button>
+    <button type="button">
         home
     </button>
     </>
--- my-portofolio/components/Navbar.js
+++ my-portofolio/components/Navbar.js
@@ -1,9 +1,13 @@
 import Button from "./Button";
+import styles from "../style/Navbar.module.css"
 
 export default function Navbar() {
     return <>
+    <div className={styles.navbar_buttons}>
     <Button />
     <Button />
-
-    </>
+    <Button />
+    <Button />
+    </div>
+   </>
 }
