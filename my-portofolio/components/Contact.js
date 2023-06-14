aec3695f5ee869b6c8b1a7cfcc9f50dd5488f683
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-14 23:17:03 +0300

    add contact data

--- my-portofolio/components/Contact.js
+++ my-portofolio/components/Contact.js
@@ -2,9 +2,16 @@ import styles from "../style/Contact.module.css"
 
 export default function Contact() {
     return <>
-    <div className={styles.contact_data}>
-        <p>diana.elena_dinu@yahoo.com</p>
+    <div className={styles.contact_details}>
+        <div className={styles.contact_data}>
+            <p>diana.elena_dinu@yahoo.com</p>
+        </div>
+        <div className={styles.contact_data}>
+            <p>Bucharest, Romania</p>
+        </div>
+        <div className={styles.contact_data}>
+            <p>https://github.com/Diana-Elen</p>
+        </div>
     </div>
-    
     </>
 }
