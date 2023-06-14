6e64ce950f5a2f0013978b5abbca896c17e6e493
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-14 22:35:48 +0300

    add button style

--- my-portofolio/components/Button.js
+++ my-portofolio/components/Button.js
@@ -1,7 +1,9 @@
+import styles from "../style/Button.module.css"
+
 export default function Button() {
     return <>
-    <button type="button">
-        home
+    <button type="button" className={styles.button_design}>
+        about me
     </button>
     </>
 }
