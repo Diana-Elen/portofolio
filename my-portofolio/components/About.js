6617a0b714cc06a2b951d089ebe0c383c40884a6
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-15 23:29:31 +0300

    update html

--- my-portofolio/components/About.js
+++ my-portofolio/components/About.js
@@ -8,14 +8,16 @@ export default function About() {
             <p className={styles.hello}>Hello, I'm Diana Dinu
             </p>
             <p>I’m passionate about front-end web development and I've graduated from the IT School course, 
-                a 5 months of intense training,where I studied: HTML, CSS, JavaScript, React, Bootstrap.
+                a 5 months of intense training, where I studied: HTML, CSS, JavaScript, React, Bootstrap.
+            </p>
+            <p>
                 I am perseverent, hard-working and love to see the results of mywork. 
+            </p>
+            <p>
                 I am familiar with the Agile methodology and would be happy to be part of a team where I can learn
-                and develop my knowledge,while working on beautiful interactive websites.
+                and develop my knowledge, while working on beautiful interactive websites.
             </p>
         </div>
     </div>
-    
-    
     </>
 }
