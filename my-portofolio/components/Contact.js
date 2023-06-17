014e8583cca63319fc3687257dd6fcfe20d9c32b
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-17 10:32:42 +0300

    update css

--- my-portofolio/components/Contact.js
+++ my-portofolio/components/Contact.js
@@ -1,7 +1,4 @@
 import styles from "../style/Contact.module.css"
-import Image from "next/image"
-import github from "../images/github.svg"
-import linkedin from "../images/linkedin.png"
 
 export default function Contact() {
     return <>
@@ -12,16 +9,7 @@ export default function Contact() {
         <div className={styles.contact_data}>
             <p>Bucharest, Romania</p>
         </div>
-        <div className={styles.contact_data}> 
-        <a href="https://www.linkedin.com/feed/" target="_blank">
-        <Image src={linkedin.src} height={50} width={50} className={styles.linkedin}>
-        </Image>
-        </a>          
-        <a href="https://github.com/Diana-Elen" target="_blank">
-        <Image src={github.src} height={50} width={50}>
-        </Image>
-        </a>
-            
+        <div className={styles.contact_data}>            
         </div>
     </div>
     </>
