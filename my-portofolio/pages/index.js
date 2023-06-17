f71ef7c35d1fe601d822fcaad7edc03f41fce747
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-17 15:27:44 +0300

    update css

--- my-portofolio/pages/index.js
+++ my-portofolio/pages/index.js
@@ -23,8 +23,8 @@ export default function Home() {
         <Navbar />
         <JobTitle />
         <About />
-        <Experience />
         <Skills />
+        <Experience />
         <Technologies />   
         <Footer />
       </div>
