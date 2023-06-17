cc4d489cb5603abb4b436999482b9b9a6e51ea65
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-17 14:28:41 +0300

    footer update

--- my-portofolio/components/Contact.js
+++ my-portofolio/components/Contact.js
@@ -2,14 +2,12 @@ import styles from "../style/Contact.module.css"
 
 export default function Contact() {
     return <>
-    <div className={styles.contact_details}>
+    <div className={styles.contact_container}>
         <div className={styles.contact_data}>
             <p>diana.elena_dinu@yahoo.com</p>
         </div>
         <div className={styles.contact_data}>
             <p>Bucharest, Romania</p>
-        </div>
-        <div className={styles.contact_data}>            
         </div>
     </div>
     </>
