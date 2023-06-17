be3fc2f0caedd725ede2e474bfdc1ddf83951556
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-17 22:53:23 +0300

    update style

--- my-portofolio/components/JobTitle.js
+++ my-portofolio/components/JobTitle.js
@@ -6,8 +6,9 @@ export default function JobTitle() {
     return <>
     <div className={styles.job_container}>
         <div className={styles.name_title}>
-            <p className={styles.hello}>Hello, I'm Diana-Elena Dinu
+            <p className={styles.hello}>Hello, 
             </p>
+            <p className={styles.hello}>I'm Diana-Elena Dinu</p>
             <p className={styles.job_title}>Front-end web developer</p>
         </div>
         <Image className={styles.image_dimension} src={my_image.src} width={200} height={200} alt="my image"></Image>
