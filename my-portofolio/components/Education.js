712afb4ab7bc05f9a94498f1b7762879c082d052
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-18 22:39:11 +0300

    update style

--- my-portofolio/components/Education.js
+++ my-portofolio/components/Education.js
@@ -3,6 +3,7 @@ import styles from "../style/Education.module.css"
 export default function Education() {
     return <>
     <div className={styles.education_container}>
+        <div className={styles.education_title}>education</div>
         <div>
             <p className={styles.education_unit}>IT School - certification</p>
             <p className={styles.study}>Front-end web development course</p>
@@ -11,18 +12,14 @@ export default function Education() {
             <p className={styles.study}>I also promoted the Ministery of Education accreditation exam.</p>
         </div>
         <div>
-            <p className={styles.education_unit}>IT School - certification</p>
-            <p className={styles.study}>Front-end web development course</p>
-            <p className={styles.study}>dec.2022 - may 2023</p>
-            <p className={styles.study}>The main technologies studied: HTML, CSS, JavaScript, React, Next, Git, Parcel, Canva, Figma.</p>
-            <p className={styles.study}>I also promoted the Ministery of Education accreditation exam.</p>
+            <p className={styles.education_unit}>POLITEHNICA UNIVERSITY OF BUCHAREST</p>
+            <p className={styles.study}>Faculty of Transports, Master degree</p>
+            <p className={styles.study}>2014</p>
         </div>
         <div>
-            <p className={styles.education_unit}>IT School - certification</p>
-            <p className={styles.study}>Front-end web development course</p>
-            <p className={styles.study}>dec.2022 - may 2023</p>
-            <p className={styles.study}>The main technologies studied: HTML, CSS, JavaScript, React, Next, Git, Parcel, Canva, Figma.</p>
-            <p className={styles.study}>I also promoted the Ministery of Education accreditation exam.</p>
+            <p className={styles.education_unit}>POLITEHNICA UNIVERSITY OF BUCHAREST</p>
+            <p className={styles.study}>Licence degree - Faculty of PowerEngineering</p>
+            <p className={styles.study}>2003 - 2008</p>
         </div>
     </div>
     </>
