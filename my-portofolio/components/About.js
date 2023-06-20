49dd6d17168e6ad9cfe9b3b488329b8aab805b97
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-20 21:46:56 +0300

    update html

--- my-portofolio/components/About.js
+++ my-portofolio/components/About.js
@@ -3,9 +3,7 @@ import styles from "../style/About.module.css"
 
 export default function About() {
     return <>
-    
-        <div className={styles.description}>
-            
+        <div className={styles.description}> 
             <p>I’m passionate about front-end web development and I've graduated from the IT School course, 
                 a 5 months of intense training, where I studied: HTML, CSS, JavaScript, React, Bootstrap.
             </p>
@@ -17,6 +15,5 @@ export default function About() {
                 and develop my knowledge, while working on beautiful interactive websites.
             </p>
         </div>
-    
     </>
 }
