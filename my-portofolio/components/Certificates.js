2a9e6fa8eee0bdb0ca245322c24b90a8d8ce031d
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-21 23:29:58 +0300

    update style

--- my-portofolio/components/Certificates.js
+++ my-portofolio/components/Certificates.js
@@ -2,9 +2,9 @@ import styles from "../style/Certificates.module.css"
 
 export default function Certificates() {
     return <>
-    <div className={styles.certifications_container}>
-        <p className={styles.certifications}>IT School certification</p>
-        <p className={styles.certifications}>Ministery of Education certification</p>
+    <div className={styles.certificates_container}>
+        <p className={styles.certificates}>IT School certification</p>
+        <p className={styles.certificates}>Ministery of Education certification</p>
     </div>    
     </>
 }
