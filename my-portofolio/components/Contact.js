3a06fe425e72a4ff308eb99d07d557094983f54c
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-25 11:21:14 +0300

    delete button

--- my-portofolio/components/Contact.js
+++ my-portofolio/components/Contact.js
@@ -13,15 +13,5 @@ export default function Contact() {
     </>
 }
 
-export function Button() {
-    function handleClick() {
-      alert('You clicked me!');
-    }
-  
-    return (
-      <button onClick={handleClick}>
-        Click me
-      </button>
-    );
-  }
+
   
--- my-portofolio/pages/index.js
+++ my-portofolio/pages/index.js
@@ -37,7 +37,6 @@ export default function Home() {
         <Experience />
         <Technologies />   
         <Certificates />
-        <Button />
         <Footer />
       </div>
     </>
