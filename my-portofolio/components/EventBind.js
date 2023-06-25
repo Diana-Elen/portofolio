68ea25dc62d046159522574c576fa2ad43f28a37
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-25 13:32:08 +0300

    update js

--- my-portofolio/components/EventBind.js
+++ my-portofolio/components/EventBind.js
@@ -1,16 +1,23 @@
 import { useState } from "react";
+import randomColor from "randomcolor";
+
 
 export default function EventBind() {
-    const [steps, setSteps] = useState(0);
+    const [steps, setSteps] = useState(10);
     /*Note the fact that we are using the arrow function here.
     We will discuss about it in binding section soon*/
     const clickHandler = () => {
       setSteps(steps + 1);
     };
+
+    const [color, changeColor] = useState("green");
+   
     return (
       <>
         <div>{steps}</div>
         <button onClick={clickHandler}>Click</button>
+        <h1 style={{ color }}> changing colors </h1>
+        <button onMouseOver = {() => changeColor(randomColor())}>click me</button>
       </>
     );
   }
