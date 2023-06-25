fff80f94d574ec908f52edbfdb397242cb391189
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-25 22:21:02 +0300

    add background color

--- my-portofolio/components/About.js
+++ my-portofolio/components/About.js
@@ -7,11 +7,9 @@ export default function About() {
             <p>I’m passionate about front-end web development and I've graduated from the IT School course, 
                 a 5 months of intense training, where I studied: HTML, CSS, JavaScript, React, Bootstrap.
             </p>
-            <p>
-                I am perseverent, hard-working and love to see the results of mywork. 
+            <p>I am perseverent, hard-working and love to see the results of my work. 
             </p>
-            <p>
-                I am familiar with the Agile methodology and would be happy to be part of a team where I can learn
+            <p>I am familiar with the Agile methodology and would be happy to be part of a team where I can learn
                 and develop my knowledge, while working on beautiful interactive websites.
             </p>
         </div>
--- my-portofolio/components/Technologies.js
+++ my-portofolio/components/Technologies.js
@@ -4,6 +4,7 @@ import html from "../images/html5-2.svg"
 import css from "../images/css-3.svg"
 import javascript from "../images/javascript-1.svg"
 import bootstrap from "../images/bootstrap-5-1.svg"
+import github from "../images/github.png"
 import styles from "../style/Technologies.module.css"
 
 
@@ -14,11 +15,12 @@ export default function Technologies() {
             Technologies
         </div>
         <div className={styles.technologies_group}>
-            <Image src={html.src} height={40} width={40} alt="linkedin image" className={styles.technology_image}></Image>
-            <Image src={css.src} height={40} width={40} alt="linkedin image" className={styles.technology_image}></Image>
-            <Image src={javascript.src} height={40} width={40} alt="linkedin image" className={styles.technology_image}></Image>
-            <Image src={bootstrap.src} height={40} width={40} alt="linkedin image" className={styles.technology_image}></Image>
-            <Image src={react.src} height={40} width={40} alt="linkedin image" className={styles.technology_image}></Image>
+            <Image src={html.src} height={40} width={40} alt="html image" className={styles.technology_image}></Image>
+            <Image src={css.src} height={40} width={40} alt="css image" className={styles.technology_image}></Image>
+            <Image src={javascript.src} height={40} width={40} alt="javascript image" className={styles.technology_image}></Image>
+            <Image src={bootstrap.src} height={40} width={40} alt="bootstrap image" className={styles.technology_image}></Image>
+            <Image src={react.src} height={40} width={40} alt="react image" className={styles.technology_image}></Image>
+            <Image src={github.src} height={40} width={40} alt="github image" className={styles.technology_image}></Image>
         </div>
     </div>
     </>
