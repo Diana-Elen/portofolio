336d4efceb68bfea667ae78352ce63222fc61014
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-25 14:32:41 +0300

    add function for image change

--- my-portofolio/components/JobTitle.js
+++ my-portofolio/components/JobTitle.js
@@ -1,9 +1,16 @@
 import Image from "next/image"
 import my_image from "../images/poza.png"
+import abstract from "../images/abstract.png"
 import styles from "../style/JobTitle.module.css"
+import { useState } from "react"
 
 export default function JobTitle() {
-    
+    const [imageSrc, setImageSrc] = useState(my_image);
+
+    // Function to change the image source
+    const changeImageSrc = () => {
+      setImageSrc(abstract);
+    }
     return <>
     <div className={styles.job_container}>
         <div className={styles.name_title}>
@@ -12,7 +19,19 @@ export default function JobTitle() {
             <p className={styles.hello}>I'm Diana-Elena Dinu</p>
             <p className={styles.job_title}>Front-end web developer</p>
         </div>
-        <Image className={styles.image_dimension} src={my_image.src} width={200} height={200} alt="my image"></Image>
+        {/*<Image className={styles.image_dimension} 
+            src={imageSrc} 
+            width={200} 
+            height={200} 
+            alt="my image">
+            </Image>
+        <button onClick={changeImageSrc}>press</button> */}
+        <Image className={styles.image_dimension}
+        src={imageSrc} 
+        alt="my img" 
+        height={200} 
+        width={200} />
+        <button onClick={changeImageSrc}>Change Image</button>
     </div>
     </>
 }
