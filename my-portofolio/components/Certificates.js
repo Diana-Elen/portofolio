75755f95e971d74b526ba19c2e056afd3d346e7b
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-26 20:20:54 +0300

    add certificates

--- my-portofolio/components/Certificates.js
+++ my-portofolio/components/Certificates.js
@@ -3,8 +3,13 @@ import styles from "../style/Certificates.module.css"
 export default function Certificates() {
     return <>
     <div className={styles.certificates_container}>
-        <p className={styles.certificates}>IT School certification</p>
-        <p className={styles.certificates}>Ministery of Education certification</p>
+        <p className={styles.certificates_title}>
+            Certificates
+        </p>
+        <div className={styles.certificates_group}>
+            <p className={styles.certificates}>IT School certification</p>
+            <p className={styles.certificates}>Ministery of Education certification</p>
+        </div>
     </div>    
     </>
 }
