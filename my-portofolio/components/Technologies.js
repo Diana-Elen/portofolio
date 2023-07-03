b645e49a63043e3fe60397a6bd9c82bfe22b8f9e
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-07-03 19:23:52 +0300

    update bootstrap

--- my-portofolio/components/Technologies.js
+++ my-portofolio/components/Technologies.js
@@ -3,7 +3,7 @@ import react from "../images/react-2.svg"
 import html from "../images/html5-2.svg"
 import css from "../images/css-3.svg"
 import javascript from "../images/javascript-1.svg"
-import bootstrap from "../images/bootstrap-5-1.svg"
+import bootstrap from "../images/react_bootstrap.svg"
 import github from "../images/github.png"
 import styles from "../style/Technologies.module.css"
 import next_img from "../images/nextjs.svg"
@@ -24,7 +24,7 @@ export default function Technologies() {
             <Image src={react.src} height={40} width={40} alt="react image" className={styles.technology_image}></Image>
             <Image src={github.src} height={40} width={40} alt="github image" className={styles.technology_image}></Image>
             <Image src={next_img.src} height={40} width={40} alt="github image" className={styles.technology_image}></Image>
-            <Image src={npm_logo.src} height={10} width={500} alt="github image" className={styles.technology_image}></Image>
+            <Image src={npm_logo.src} height={40} width={500} alt="github image" className={styles.technology_image}></Image>
         </div>
     </div>
     </>
