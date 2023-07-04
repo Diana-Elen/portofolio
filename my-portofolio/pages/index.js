5d0c0e036a3bdae9e6c3f61181a3655e3a320dca
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-07-04 22:07:02 +0300

    useref

--- my-portofolio/pages/index.js
+++ my-portofolio/pages/index.js
@@ -11,6 +11,7 @@ import Footer from '@/components/Footer'
 import Education from '@/components/Education'
 import Certificates from '@/components/Certificates'
 import Form from '@/components/Form'
+import Navbar2 from '@/components/Navbar2'
 
 
 export default function Home() {
@@ -35,6 +36,7 @@ export default function Home() {
           <Technologies />   
           <Certificates />
           <Form />
+          <Navbar2 />
         </div>
         <Footer />
       </div>
